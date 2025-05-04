a1088ac2f384c6b361eb51e0f53d49e07db231a4
David Elkind <david.elkind@oracle.com>
2025-05-04 21:17:05 +0300

    Initial commit

---
+++ kazimierz_input_final/js/data.js
@@ -0,0 +1,26 @@
+const stops = [
+  {
+    title: "Площадь Вольница и ратуша Казимежа",
+    description: "Центральная площадь бывшего города Казимеж, с ратушей и музеем.",
+    audio: "audio/001_plac_wolnica.mp3",
+    image: "images/001.jpg",
+    lat: 50.0486624,
+    lon: 19.9434852
+  },
+  {
+    title: "Улица Йозефа",
+    description: "Главная торговая артерия еврейского Казимежа — с хедерами и галереями.",
+    audio: "audio/002_ulica_jozefa.mp3",
+    image: "images/002.jpg",
+    lat: 50.0513067,
+    lon: 19.9474002
+  },
+  {
+    title: "Улица Шерока и синагога Рема",
+    description: "Духовное сердце квартала с действующей синагогой и кладбищем.",
+    audio: "audio/003_szeroka_synagogue.mp3",
+    image: "images/003.jpg",
+    lat: 50.0526639,
+    lon: 19.9474024
+  },
+];
