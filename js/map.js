87a80954a58ab1fc10f1af19af2f7e113181286c
David Elkind <david.elkind@gmail.com>
2025-06-01 14:04:55 +0300

    Upload tour update

--- js/map.js
+++ js/map.js
@@ -80,13 +80,10 @@ if (typeof L !== 'undefined') {
     // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     //     attribution: '&copy; OpenStreetMap'
     // }).addTo(map);
-    // L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-    //     maxZoom: 20,
-    //     subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-    // })
-    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{x}/{y}', {
-        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-    }).addto(map);
+    L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+        maxZoom: 20,
+        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    }).addTo(map);
     markers = stops.map((s, i) => {
         const m = L.marker([s.lat, s.lon]).addTo(map);
         m.bindTooltip(String(s.id), {
