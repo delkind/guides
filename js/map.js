fc639027e4cb8360bd8020e811ecf560134c865d
David Elkind <david.elkind@gmail.com>
2025-06-01 14:00:16 +0300

    Upload tour update

--- js/map.js
+++ js/map.js
@@ -77,14 +77,16 @@ if (typeof L !== 'undefined') {
     map = L.map('map');
     const bounds = L.latLngBounds(stops.map(s => [s.lat, s.lon]));
     map.fitBounds(bounds, {padding: [10, 10]});
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; OpenStreetMap'
-    }).addTo(map);
+    // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //     attribution: '&copy; OpenStreetMap'
+    // }).addTo(map);
     // L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
     //     maxZoom: 20,
     //     subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
     // })
-
+    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    });
     markers = stops.map((s, i) => {
         const m = L.marker([s.lat, s.lon]).addTo(map);
         m.bindTooltip(String(s.id), {
