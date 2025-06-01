d2260f82ef2975f6cce00df45c6fb1e54dd9f689
David Elkind <david.elkind@gmail.com>
2025-06-01 14:02:30 +0300

    Upload tour update

--- js/map.js
+++ js/map.js
@@ -84,7 +84,7 @@ if (typeof L !== 'undefined') {
     //     maxZoom: 20,
     //     subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
     // })
-    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{x}/{y}', {
         attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
     });
     markers = stops.map((s, i) => {
