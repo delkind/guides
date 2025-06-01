20993c0c445cd0e13afe436153412c68b024ed24
David Elkind <david.elkind@oracle.com>
2025-06-01 11:52:10 +0300

    Tours updated

--- js/map.js
+++ js/map.js
@@ -80,6 +80,10 @@ if (typeof L !== 'undefined') {
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; OpenStreetMap'
     }).addTo(map);
+    // L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+    //     maxZoom: 20,
+    //     subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    // })
 
     markers = stops.map((s, i) => {
         const m = L.marker([s.lat, s.lon]).addTo(map);
